f6a494e8810435654b0bed7c4f68229244e04838
Dmitriy Paunin <D.M.Paunin@gmail.com>
2013-01-08 16:23:15 +0400

    Update js/server/remoteall_server.js

--- js/server/remoteall_server.js
+++ js/server/remoteall_server.js
@@ -2,7 +2,7 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-app.listen(8888);
+app.listen(80);
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
