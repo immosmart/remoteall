4697333f958a9738c386982a8cfd61ffa27992d5
paunin <paunin@immo.ru>
2013-02-18 11:46:27 +0400

    config in file

---
+++ js/config.example.js
@@ -0,0 +1,9 @@
+var REMOTE_ALL_CONFIG = {
+    appId: 'appNan',
+    uniqueSessionId: 'SecretStringForConnection',
+    host: {
+        protocol:'http',
+        domain: 'dev.remoteall.org',
+        port: '8888'
+    }
+}
