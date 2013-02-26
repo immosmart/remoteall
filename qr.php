79be06e197fca4787a224d204593496236b882f1
paunin <paunin@immo.ru>
2013-02-26 16:05:07 +0400

    fixes

--- qr.php
+++ qr.php
@@ -1,4 +1,4 @@
 <?php 
 require("./lib/phpqrcode/qrlib.php");
-QRcode::png(!empty($_GET['url'])?'http://remoteall.org/'.$_GET['url']:'http://remoteall.org',false,15);
+QRcode::png(!empty($_GET['url'])?'http://remoteall.org/'.$_GET['url']:'http://remoteall.org',false,QR_ECLEVEL_L,5,5);
 ?>
