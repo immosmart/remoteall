f2d6f085b87679cb4737536be743dd91beec8e27
Dmitriy <paunin@immo.ru>
2013-03-19 16:57:05 +0400

    test commit

--- qr.php
+++ qr.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 require("./lib/phpqrcode/qrlib.php");
 QRcode::png(!empty($_GET['url']) ? 'http://'.$_SERVER['HTTP_HOST'].'/' . $_GET['url'] : 'http://'.$_SERVER['HTTP_HOST'], false, QR_ECLEVEL_L, 7, 5);
 ?>
