926c5b98500d723c3aa72e65a6ac25473d7dba74
poletsky <poletsky@immo.ru>
2013-04-10 14:25:21 +0400

    on off implemented rooms implemented member_data implemented

--- js/server/remoteall_server.js
+++ js/server/remoteall_server.js
@@ -4,7 +4,8 @@ var app = require('http').createServer(handler)
     , io = require('socket.io').listen(app)
     , io_client = require('socket.io-client')
     , fs = require('fs')
-    , qs = require('querystring')
+    , qs = require('querystring'),
+    _ = require('lodash');
 
 app.listen(REMOTE_ALL_SERVER_CONFIG.host.port);
 
@@ -18,12 +19,15 @@ function handler(req, res) {
             });
             req.on('end', function () {
                 var POST = qs.parse(body);
+                console.log(POST);
                 var app = POST.app ? POST.app : null;
                 var session_id = POST.session_id ? POST.session_id : null;
                 var emit_data = POST.emit_data ? POST.emit_data : null;
 
                 if (!app || !session_id || !emit_data) {
-                    res.writeHead(400);
+                    res.writeHead(400, {
+                        'Access-Control-Allow-Origin': '*'
+                    });
                     res.end('Need all POST params app,session_id,emit_data');
                 }
 
@@ -39,15 +43,21 @@ function handler(req, res) {
 
                 s_socket.emit('send_code', emit_data_parsed);
 
-                res.writeHead(200);
+                res.writeHead(200, {
+                    'Access-Control-Allow-Origin': '*'
+                });
                 res.end('OK');
             });
         } else {
-            res.writeHead(400);
+            res.writeHead(400, {
+                'Access-Control-Allow-Origin': '*'
+            });
             res.end('Only POST requests');
         }
     } else {
-        res.writeHead(404);
+        res.writeHead(404, {
+            'Access-Control-Allow-Origin': '*'
+        });
         res.end('');
     }
 
@@ -63,11 +73,14 @@ function handler(req, res) {
 //        });
 
 }
-
+var rooms = {};
 io.sockets.on('connection', function (socket) {
 
     var app = 'share'
     var session_id = 'share';
+    var my_rooms = {};
+    var my_member_data;
+
     socket.join(app + '_' + session_id);
 
     //set main Session/Room that used for communications
@@ -79,21 +92,61 @@ io.sockets.on('connection', function (socket) {
     });
 
     //connect to additional session
-    socket.on('add_session', function (_session_id) {
+    socket.on('add_session', function (_session_id, member_data) {
         console.log('add session ' + _session_id);
-        socket.join(app + '_' + _session_id);
+        var fullSessID = app + '_' + _session_id;
+        if (member_data && !my_rooms[_session_id]) {
+            my_member_data = member_data;
+            my_rooms[_session_id] = true;
+            if (!rooms[fullSessID]) {
+                rooms[fullSessID] = [];
+            }
+            console.log('sending members list');
+            console.log(rooms[fullSessID]);
+            socket.emit('members_list', rooms[fullSessID], _session_id);
+            rooms[fullSessID].push(member_data);
+            io.sockets.in(fullSessID).emit('on', member_data, _session_id);
+        }
+
+        socket.join(fullSessID);
     });
 
     //disconnect from additional session
     socket.on('remove_session', function (_session_id) {
         console.log('add session ' + _session_id);
-        socket.leave(app + '_' + _session_id);
+        var fullSessID = app + '_' + _session_id;
+        socket.leave(fullSessID);
+        if (my_rooms[_session_id]) {
+            rooms[fullSessID] = _.filter(rooms[fullSessID], function (member_data) {
+                return member_data !== my_member_data;
+            });
+            if (!rooms[fullSessID].length) {
+                delete rooms[fullSessID]
+            }
+            io.sockets.in(fullSessID).emit('off', my_member_data, _session_id);
+        }
     });
 
 
     socket.on('send_code', function (data, _session_id) {
         var ses_id = _session_id ? _session_id : session_id;
-        io.sockets.in(app + '_' + ses_id).emit('recive_code', {data: data, session_id: ses_id})
+        io.sockets.in(app + '_' + ses_id).emit('recive_code', data, ses_id);
+    });
+
+    socket.on('disconnect', function () {
+        for (var _session_id in my_rooms) {
+            var session_id = app + '_' + _session_id;
+            io.sockets.in(session_id).emit('off', my_member_data, _session_id);
+
+            rooms[session_id] = _.filter(rooms[session_id], function (member_data) {
+                return member_data !== my_member_data;
+            });
+
+            if (!rooms[session_id].length) {
+                delete rooms[session_id]
+            }
+        }
+        my_rooms = {};
     });
 });
 
